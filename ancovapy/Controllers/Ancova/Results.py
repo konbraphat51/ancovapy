40bbbf64b331b52d7b23b83dda47b7bec499f281
konbraphat <konbraphat@gmail.com>
2026-02-01 00:04:52 +0900

    homogeneity

--- ancovapy/Controllers/Ancova/Results.py
+++ ancovapy/Controllers/Ancova/Results.py
@@ -3,5 +3,33 @@ from dataclasses import dataclass
 @dataclass
 class AncovaResults:
     """Dataclass to report ANCOVA results."""
-    # TODO: implement ANCOVA results fields
-    pass
+
+    @dataclass
+    class AssumptionResults:
+        """Dataclass to report assumption test results."""
+
+        @dataclass
+        class HomogeneityOfRegressionSlopes:
+            """Dataclass to report homogeneity of regression slopes test results.
+            
+            if `passed==False`, use Heterogeneous Regression ANCOVA model.
+            """
+
+            @dataclass
+            class InteractionTerm:
+                """significance of interaction term in full model"""
+
+                target_terms: list[str]
+                f_statistic: float
+                df: int
+                p_value: float
+                rejected: bool
+
+            interaction_terms: list[InteractionTerm]
+
+            passed: bool
+            """True if all interaction terms are not significant."""
+
+        homogeneity_of_regression_slopes: HomogeneityOfRegressionSlopes
+
+    assumption: AssumptionResults
