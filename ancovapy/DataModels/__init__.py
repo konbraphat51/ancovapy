d46537a0d50ff9574238c117a77ed30808fb8ab5
konbraphat <konbraphat@gmail.com>
2026-01-31 21:24:48 +0900

    add __init__.py to DataModels for module exports

---
+++ ancovapy/DataModels/__init__.py
@@ -0,0 +1,9 @@
+from ancovapy.DataModels.Formula import LinearFormula
+from ancovapy.DataModels.Variables import Variable, QuantitativeVariable, CategoricalVariable
+
+__all__ = [
+    "Variable",
+    "QuantitativeVariable",
+    "CategoricalVariable",
+    "LinearFormula",
+]
